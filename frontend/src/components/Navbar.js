f0e0b8bea9ff3a3faa0b3a119b10644aa88d5cce
Aditya Chavan <adityachavan677@gmail.com>
2020-09-11 20:23:27 +0530

    getCookie function completed in navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -125,6 +125,21 @@ const Navbar = () => {
   const getCookie = (name) => {
     let dc = document.cookie;
     let prefix = name + "=";
+    let begin = dc.indexOf(";" + prefix);
+    if (begin === -1) {
+      begin = dc.indexOf(prefix);
+      if (begin !== 0) {
+        return null;
+      }
+    } else {
+      begin += 2;
+      var end = document.cookie.indexOf(";", begin);
+      if (end === -1) {
+        end = dc.length;
+      }
+    }
+
+    return decodeURI(dc.substring(begin + prefix.length, end));
   };
 
   const fetchProfile = () => {
